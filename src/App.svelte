2afcf34942b6f11b63d2732555f116d25342185f
Fermin Rodriguez Penelas <frodriguezpenelas@gmail.com>
2023-10-25 18:50:44 -0300

    saco signo pesos a la hora en el footer

--- src/App.svelte
+++ src/App.svelte
@@ -140,7 +140,7 @@
 
 <a id="cafecito" href='https://cafecito.app/ferminrp' rel='noopener' target='_blank'><img srcset='https://cdn.cafecito.app/imgs/buttons/button_3.png 1x, https://cdn.cafecito.app/imgs/buttons/button_3_2x.png 2x, https://cdn.cafecito.app/imgs/buttons/button_3_3.75x.png 3.75x' src='https://cdn.cafecito.app/imgs/buttons/button_3.png' alt='Invitame un café en cafecito.app' /></a>
 <div style="margin-top: 20px;">
-  <small>Última actualización: ${new Date().toLocaleTimeString()}</small><br>
+  <small>Última actualización: {new Date().toLocaleTimeString()}</small><br>
   <small>Data provista por el api de criptoya.com</small><br>
   <small>Sugerencias a <a href="https://x.com/ferminrp">X.com/ferminrp</a></small>
 </div>
